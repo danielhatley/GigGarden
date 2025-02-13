525f11b8641f1d23fe4e622a32605da632f07b38
danie <danie@DESKTOP-16E5DVG>
2025-02-12 17:45:26 -0800

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using GigGarden.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GigGarden.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+       
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿
+
+using GigGarden.Data;
+using GigGarden.Models;
+
+namespace GigGarden.Repositories;
+
+public class UserRepository
+{
+    private readonly DataContextDapper _dapper;
+
+    public UserRepository(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    public async Task<List<User>> GetUser(int? userId, bool showDeleted = false)
+    {
+
+        return new List<User>();
+    }
+}
+
