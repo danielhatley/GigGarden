2ee7f406e930c75720470e8253562d8cdb43b85d
Daniel Hatley <daniel.hatley@gmail.com>
2025-02-20 12:18:25 -0800

    created Helper folder with TimeHelper for timestamping and removed from BaseAttributes and BaseLinkAttributes

--- Models/Base/BaseAttributes.cs
+++ Models/Base/BaseAttributes.cs
@@ -2,26 +2,14 @@
 {
     public class BaseAttributes
     {
-
-        // Automatically set to current Unix timestamp when a new entity is created.
-        public long CreatedAt { get; set; } = GetCurrentUnixTimestamp();
-
-        // Nullable properties for timezone offset, updates, and deletions.
+        public long CreatedAt { get; set; }
         public int? CreatedOffset { get; set; }
         public int CreatedBy { get; set; }
-        public long? UpdatedAt { get; set; } // = GetCurrentUnixTimestamp();    ?
+        public long? UpdatedAt { get; set; } 
         public int? UpdatedOffset { get; set; }
         public int? UpdatedBy { get; set; }
-        public long? DeletedAt { get; set; } // = GetCurrentUnixTimestamp();    ?
+        public long? DeletedAt { get; set; } 
         public int? DeletedOffset { get; set; }
         public int? DeletedBy { get; set; }
-
-        // Helper method to convert current UTC time to a Unix timestamp (in seconds) - ***create it's own helper file?
-        public static long GetCurrentUnixTimestamp()
-        {
-            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        }
-
-
     }
 }
--- Models/Base/BaseLinkAttributes.cs
+++ Models/Base/BaseLinkAttributes.cs
@@ -2,20 +2,11 @@
 {
     public class BaseLinkAttributes
     {
-        // Automatically set to current Unix timestamp when a new entity is created.
-        public long CreatedAt { get; set; } = GetCurrentUnixTimestamp();
-
-        // Nullable properties for timezone offset, creation, and deletions.
+        public long CreatedAt { get; set; }
         public int? CreatedOffset { get; set; }
         public int CreatedBy { get; set; }
         public long? DeletedAt { get; set; }
         public int? DeletedOffset { get; set; }
         public int? DeletedBy { get; set; }
-
-        // Helper method to convert current UTC time to a Unix timestamp (in seconds).
-        public static long GetCurrentUnixTimestamp()
-        {
-            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        }
     }
 }
